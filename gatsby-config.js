824a4abac3ad702b53721f525239ae165dd80d06
dan2811 <djordandrums@gmail.com>
2021-11-24 13:41:08 +0000

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Dan`,
+    siteTitleAlt: `Daniel Jordan - Full Stack Web Developer`,
+    siteHeadline: `Daniel Jordan - Full Stack Web Developer`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `My portfolio`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Dan`,
   },
   plugins: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Daniel Jordan`,
+        short_name: `Dan`,
+        description: `Full stack software developer`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
